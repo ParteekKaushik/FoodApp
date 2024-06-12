7a5ca20473c835d480582988ab691d481d7e35a3
ParteekKaushik <kaushikparteekofficial@gmail.com>
2024-06-12 23:10:22 +0530

    Added SearchBar and Cards to Body

--- App.js
+++ App.js
@@ -17,15 +17,51 @@ const Header = () => {
         </ul>
       </div>
     </div>
-  );
+  )
 };
 
+const Body = () => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+  
+        <div className="res-container">
+          <div className="res-item">
+              <ResturantCard /> 
+              <ResturantCard /> 
+              <ResturantCard /> 
+              <ResturantCard /> 
+              <ResturantCard /> 
+              <ResturantCard /> 
+              <ResturantCard /> 
+              <ResturantCard /> 
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  const ResturantCard = () => {
+    return <div className="res-card">
+      <div className="res-img">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/978358a0d0caba4f4555b8e2147b467d" />
+      </div>
+  
+      <div className="res-details">
+        <h3>Pizza Hut</h3>
+        <h4>Pizzas</h4>
+        <h4>4.4 Star rating</h4>
+        <h4>38 minutes</h4>
+      </div>
+    </div>;
+  };
 
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
