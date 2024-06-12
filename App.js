445b41eac939e0a82a8414482edb07ba2962f71d
ParteekKaushik <kaushikparteekofficial@gmail.com>
2024-06-12 23:00:53 +0530

    Add Project to git (added only header)

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="" />
+      </div>
+
+      <div className="navItems">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
